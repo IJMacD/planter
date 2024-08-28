c8a333b065f8525563f3c8280f214242a8cea841
Iain MacDonald <IJMacD@gmail.com>
2024-08-28 23:35:14 +0800

    Add CBUS

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from pyftdi.ftdi import Ftdi
 from pyftdi.gpio import GpioAsyncController
 from time import sleep
 from argparse import ArgumentParser
-import datetime
+from datetime import datetime, timedelta
 
 url = environ.get('FTDI_DEVICE', 'ftdi:///1')
 
@@ -15,19 +15,36 @@ args = parser.parse_args()
 print("Running planter for {0} seconds".format(args.duration))
 
 def debug_print(message):
-    print("{0} {1}".format(datetime.datetime.now().isoformat(), message))
+    print("{0} {1}".format(datetime.now().isoformat(), message))
 
+# DBUS
 gpio = GpioAsyncController()
 gpio.configure(url, direction=0xff)
 # port = gpio.get_gpio()
 
+# CBUS
+Ftdi.set_cbus_direction(0xff, 0xff)
+
+def set_pins(level = "low"):
+    if level == "high":
+        gpio.write(0xff)            # DBUS
+        Ftdi.set_cbus_gpio(0xff)    # CBUS
+    else:
+        gpio.write(0x00)            # DBUS
+        Ftdi.set_cbus_gpio(0x00)    # CBUS
+
 debug_print("Connected")
 
-gpio.write(0xff)
+start_time = datetime.now()
+stop_time = start_time + timedelta(seconds=args.duration)
+
+set_pins("high")
 debug_print("on")
 
-sleep(args.duration)
-gpio.write(0x00)
+while datetime.now() < stop_time:
+    sleep(1)
+
+set_pins("low")
 debug_print("off")
 
 debug_print("Done")
